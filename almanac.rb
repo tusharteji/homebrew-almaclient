ff9e0bf9bb55ae02d0326b51bd4936e7ec54cad4
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-14 15:31:59 -0700

    Added begin rescue in install
    sdc
    sdcsd
    exit
    quit
    exit()

--- almanac.rb
+++ almanac.rb
@@ -5,7 +5,27 @@ class Almanac < Formula
   sha256 "cfcbd878c3425c5ba741e5b2cdf9723285c4c79ed517cea1da6bcbd3e963863d"
 
   def install
-    bin.install "almanac"
+    begin
+      system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+      # system "cmake", ".", *std_cmake_args
+      system "make", "install" # if this fails, try separate make/make install steps
+    rescue
+      bin.install "almanac"
   end
 
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test almanac`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
