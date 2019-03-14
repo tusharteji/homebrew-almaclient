2391b90e83dc85f41de4907438fde88da2a60306
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-14 10:14:09 -0700

    Added formula

---
+++ almanac.rb
@@ -0,0 +1,11 @@
+class Almanac < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/tusharteji/almanac/archive/v1.0.0.tar.gz"
+  sha256 "cfcbd878c3425c5ba741e5b2cdf9723285c4c79ed517cea1da6bcbd3e963863d"
+
+  def install
+	bin.install "almanac"
+  end
+
+end
