e6ed81b2e55e0906444692f7a70fdeea4cc9914a
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-14 15:40:52 -0700

    Added end for begin-rescue

--- almanac.rb
+++ almanac.rb
@@ -14,18 +14,11 @@ class Almanac < Formula
       system "make", "install" # if this fails, try separate make/make install steps
     rescue
       bin.install "almanac"
+    end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test almanac`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
+
 end
