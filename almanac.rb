535358b8481fa0232c5530ad0740e7cac2841ab1
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-15 10:11:47 -0700

    Another try

--- almanac.rb
+++ almanac.rb
@@ -5,7 +5,7 @@ class Almanac < Formula
   sha256 "cfcbd878c3425c5ba741e5b2cdf9723285c4c79ed517cea1da6bcbd3e963863d"
 
   def install
-    bin.install "bin/almanac"
+    bin.install "almanac"
   end
 
 end
