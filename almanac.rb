d64032746949bd85293c344bb3ea663794151cee
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-14 18:08:42 -0700

    Another try

--- almanac.rb
+++ almanac.rb
@@ -5,16 +5,12 @@ class Almanac < Formula
   sha256 "cfcbd878c3425c5ba741e5b2cdf9723285c4c79ed517cea1da6bcbd3e963863d"
 
   def install
-    begin
-      system "./configure", "--disable-debug",
+    system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-      # system "cmake", ".", *std_cmake_args
-      system "make", "install" # if this fails, try separate make/make install steps
-    rescue
-      bin.install "almanac"
-    end
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
