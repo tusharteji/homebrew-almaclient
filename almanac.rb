b02e1a7901fda19bb9daeb2f674927078c397d5b
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-15 11:06:28 -0700

    Blah

--- almanac.rb
+++ almanac.rb
@@ -5,8 +5,7 @@ class Almanac < Formula
   sha256 "cfcbd878c3425c5ba741e5b2cdf9723285c4c79ed517cea1da6bcbd3e963863d"
 
   def install
-    mv Dir.glob("almanac*"), "almanac"
-    bin.install "almanac"
+    bin.install "almanac.py"
   end
 
 end
