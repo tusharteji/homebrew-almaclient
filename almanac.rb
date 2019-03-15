16f1e47738172c064dcca363825f59cbc7405a1c
Tushar Teji <tusharteji@MacBook-Pro-2.local>
2019-03-15 12:53:32 -0700

    No change

--- almanac.rb
+++ almanac.rb
@@ -1,11 +1,11 @@
 class Almanac < Formula
   desc ""
   homepage ""
-  url "https://github.com/tusharteji/almanac/archive/v1.0.0.tar.gz"
-  sha256 "cfcbd878c3425c5ba741e5b2cdf9723285c4c79ed517cea1da6bcbd3e963863d"
+  url "https://github.com/tusharteji/almanac/archive/v1.0.1.tar.gz"
+  sha256 "92f9be539542fdc30705e2b2ca5d4992c07b65b2a2586100a3e222ddcbe2adfa"
 
   def install
-    bin.install "almanac.py"
+    bin.install "almanac"
   end
 
 end
